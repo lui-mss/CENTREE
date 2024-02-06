22cd959667d57346e176f662178a3463b69c02dc
yolanda martinez priego <yulielx@gmail.com>
2024-02-06 16:34:47 +0000

    Index with some front. Dropdown to be added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,12 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @icons = [
+      "<i class=\"fa-solid fa-hospital\"></i>",
+      "<i class=\"fa-solid fa-person-shelter\"></i>",
+      "<i class=\"fa-solid fa-scale-balanced\"></i>",
+      "<i class=\"fa-solid fa-graduation-cap\"></i>",
+      "<i class=\"fa-solid fa-briefcase\"></i>"
+    ]
   end
 end
