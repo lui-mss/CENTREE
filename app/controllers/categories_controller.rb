cfc8f9a3420ffc58e916e765e99e771229c60b0c
yolanda martinez priego <yulielx@gmail.com>
2024-02-06 18:09:37 +0000

    Dropdown changed to a select form and preparation work to update categories according to city selected

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,14 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+
+    # @offer = @offers.find(params[:city])
+
+    # IF STATEMENT OR JAVASCRIPT CONTROLLER?
     @categories = Category.all
     @icons = [
-      "<i class=\"fa-solid fa-hospital\"></i>",
       "<i class=\"fa-solid fa-person-shelter\"></i>",
+      "<i class=\"fa-solid fa-hospital\"></i>",
       "<i class=\"fa-solid fa-scale-balanced\"></i>",
       "<i class=\"fa-solid fa-graduation-cap\"></i>",
       "<i class=\"fa-solid fa-briefcase\"></i>"
