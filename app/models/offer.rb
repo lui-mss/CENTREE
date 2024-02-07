16f56dd1c1c06cc8c2150d36789dd572cc8d74b3
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2024-02-07 12:42:07 +0100

    finished with validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,10 @@ class Offer < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :upvotes, numericality: { greater_or_equal_to: 0 }
+  validates :country, presence: true
+  validates :city, presence: true
 end
