f6ca8e4aba6e68a18c54f7705cc2c09640eb4438
luisa santos <1analuisantos@gmail.com>
2024-02-07 16:46:48 +0000

    homepage filter by country and category index filter by city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_155048) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_161058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_155048) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.index ["category_id"], name: "index_offers_on_category_id"
